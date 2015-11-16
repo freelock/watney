795d029673464e94f41685f28fe2339c2a0f6541
John Locke <john@freelock.com>
2015-11-15 18:17:53 -0800

    Add example config file

---
+++ config-dist.js
@@ -0,0 +1,9 @@
+
+var config = {
+    myUserId: "@bot:matrix.freelock.com",
+    myAccessToken: "",
+    admins: ['don','chris','rafael','john'],
+    stateName: 'com.freelock.project'
+}
+
+module.exports = config;
