399d390ee14d261293f1b0fb5f395cfa652c4189
John Locke <john@freelock.com>
2016-03-25 00:08:44 -0700

    Modified to send git commits to salt

--- test.js
+++ test.js
@@ -543,17 +543,17 @@ function releaseNotes(event,room,body) {
  * Git message: "alias: refs/heads/branchname updated. new: 858b385a8a06... old: 3e1826ee2.... .
  */
 function gitCommit(event, room, body) {
-   var regexp = /^(.*):\ (.*([^\/]*))\ updated\.\ new:\ (.*)\ old:\ (.*)\ .$/;
+   var regexp = /^(.*):\ (.*\/([^\/]*))\ updated\.\ new:\ (.*)\ old:\ (.*)\ .$/;
    var matches = body.match(regexp);
     var data = {
         old: matches[5],
         new: matches[4],
         ref: matches[2],
         branch: matches[3]
-    }
+    };
 
     var exec = require('child_process').exec;
-    exec("sudo /usr/bin/salt-call event.fire_master '"+data.toString()+"' fl/git/"+matches[1],
+    exec("sudo /usr/bin/salt-call event.fire_master '"+ JSON.stringify(data)+"' fl/git/"+matches[1],
     function(err, stdout, stderr){
         if (!err) {
             console.log(stdout);
