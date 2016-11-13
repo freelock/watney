971f0bea4a36d116e99c8cd03e0dbeca64d06121
John Locke <john@freelock.com>
2016-11-13 12:33:46 -0800

    Protect deployment to production, based on results of a nightly check. Add state based on nightly check.

--- config-dist.js
+++ config-dist.js
@@ -3,8 +3,10 @@ var config = {
     myUserId: "@bot:matrix.freelock.com",
     myAccessToken: "",
     admins: ['don','chris','rafael','john'],
+    adminHomeServer: 'matrix.freelock.com',
     stateName: 'com.freelock.project',
     releaseName: 'com.freelock.release',
+    defaultReleaseManager: 'john',
     reminderName: 'com.freelock.reminder',
     concourseDir: '/path/to/pipelines',
     concoursePipelineCmd: './set_pipeline.sh',
@@ -18,7 +20,7 @@ var config = {
         path: '/auriga/store/',
         port: 443
     },
-    homeroom: '!hXysgSAERKRcnuFn:matrix.freelock.com'
+    homeroom: '!hXysgSAERKRcnuFn:matrix.freelock.com',
     deployroom: '!hXysgSAERKRcnuFn:matrix.freelock.com'
 };
 
