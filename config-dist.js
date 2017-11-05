fd67945df599161fe6cd270835b0a31d076753cc
John Locke <john@freelock.com>
2017-11-04 17:18:18 -0700

    Add case links to release

--- config-dist.js
+++ config-dist.js
@@ -21,7 +21,13 @@ var config = {
         port: 443
     },
     homeroom: '!hXysgSAERKRcnuFn:matrix.freelock.com',
-    deployroom: '!hXysgSAERKRcnuFn:matrix.freelock.com'
+    deployroom: '!hXysgSAERKRcnuFn:matrix.freelock.com',
+    sources: {
+        taiga: 'https://taiga.freelock.com/project/{alias}/{itemtype}/{id}',
+        auriga: 'https://intranet.freelock.com/auriga2/task/{id}',
+        carina: 'https://projects.freelock.com/{alias}/node/{id}',
+        atrium: 'https://intranet.freelock.com/atrium/{alias}/node/{id}'
+    }
 };
 
 module.exports = config;
